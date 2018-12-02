58b81055000175ac08ed870ea763c94047fd3331
kduane <karl.duane@gmail.com>
2018-12-02 15:30:56 -0500

    first commit

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    sequence(:email) { |n| "gamefan#{n}@gmail.com" }
+    password { 'password' }
+    password_confirmation { 'password' }
+    role { 'user' }
+   end
+end

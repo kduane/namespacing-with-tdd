8b938bd07ee721d4d4171575a99dbb96473ad000
kduane <karl.duane@gmail.com>
2018-12-02 17:07:05 -0500

    added admin authorization

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,19 @@
 class Admin::UsersController < ApplicationController
+  before_action :authorize_admin!
+
   def index
+    @users = User.all
+  end
+
+  private
+
+  def authorize_admin!
+    # binding.pry
+    if current_user.role == "admin"
+    else
+      flash[:notice] = 'You are not authorized.'
+      redirect_to :root
+    end
   end
 
 end

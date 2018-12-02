bcb5613a57743c61f9ae33f3f912d3ec6c51f076
kduane <karl.duane@gmail.com>
2018-12-02 17:11:32 -0500

    refactored authorize admin for stability and convention

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery unless: -> { request.format.json? }
+
+  private
+
+  def authorize_admin!
+    # binding.pry
+    if current_user.nil? or !current_user.is_admin?
+      flash[:notice] = 'You are not authorized to view this content.'
+      redirect_to :root
+    end
+  end
+
 end
